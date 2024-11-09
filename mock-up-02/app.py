532f835bf284caa41d10e1939a37684c5c33344a
yutadd <spyk4m49@gmail.com>
2024-11-09 14:53:39 +0900

    [delete]apikey from app.py

--- mock-up-02/app.py
+++ mock-up-02/app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request, jsonify
 import requests
-
+import os
 app = Flask(__name__)
 
-API_KEY = '1c33c613c2357110a08a8964f4aa621f'  # ここにOpenWeatherMapのAPIキーを入力
-
+API_KEY = os.environ.get('OPENWEATHERMAP_API_KEY')  # 環境変数からAPIキーを取得
 @app.route('/')
 def index():
     return render_template('index.html')
